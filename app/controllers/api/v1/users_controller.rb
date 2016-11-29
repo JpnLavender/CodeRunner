145780fa8e15adaedee1a665b3769e61aa164a81
iYaipH <irimamekun@gmail.com>
2016-11-29 23:23:04 +0900

    Create User Model

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,22 @@
 class Api::V1::UsersController < ApplicationController
   def index
+    User.all
   end
   def create
+    user =  User.new(user_params)
+    if user.save
+      render :show, status: :created
+    else
+      render :show, status: :error
+    end
   end
   def update
+    User.find_by(id: params[:id])
+  end
+
+  private
+  def user_params
+    params.permit(:name, :my_github_url, :mail, :password, :password_confirmation)
   end
 end
+
